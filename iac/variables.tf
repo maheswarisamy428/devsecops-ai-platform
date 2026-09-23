4122ab8e8e5d374ace4ebadad8cb46a3b4f9fc91
user <maheswarisamy428@gmail.com>
2026-09-23 03:46:03 +0200

    fix tflint

--- iac/variables.tf
+++ iac/variables.tf
@@ -34,8 +34,3 @@ variable "tags" {
     purpose    = "simulated-ai-microservice"
   }
 }
-
-variable "aks_api_server_authorized_ip_ranges" {
-  type        = list(string)
-  description = "CIDR ranges allowed to access the AKS API server"
-}
