cd5201dc8438fb5b33b6e12804d290d153303e33
user <maheswarisamy428@gmail.com>
2026-09-26 15:14:48 +0200

    test vulnerabilities

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.13-slim
+FROM python:latest
 
 ENV PYTHONDONTWRITEBYTECODE=1 \
     PYTHONUNBUFFERED=1
--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ async def record_http_request_metrics(
     call_next,
 ) -> Response:
     """Record request metrics for every HTTP request."""
+    OPENAI_API_KEY="sk-proj-000000000000000000000000000000000000000000000000"
     response = await call_next(request)
 
     HTTP_REQUESTS.labels(
